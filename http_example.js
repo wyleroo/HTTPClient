294906aa8a730f9b9f7d928b4102eb92d75a60e1
Wyatt <thewyatt@gmail.com>
2017-03-13 09:38:35 +0000

    Reformatting stream output to print chunk lengths

--- http_example.js
+++ http_example.js
@@ -10,7 +10,10 @@ var options = {
 
 var callback = function(response) {
   console.log('In response handler callback!');
-  console.log('Response', response);
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENgth] ', + chunk.length + ' --');
+    console.log(chunk.toString());
+  })
 }
 
 console.log("I'm about to make the request!");
